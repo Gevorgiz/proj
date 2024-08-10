5e43b02e382481537118a8b596e680b2482ec915
Zhan Gevorgiz <zhangevorgiz@gmail.com>
2024-08-10 09:07:27 +0300

    Secon commit

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -9,4 +9,11 @@ public class MainApplication {
         System.out.println("3333 Hello world");
 
     }
+
+    public static void doSomething (){
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+
+    }
 }
