594261b8481f8ae9859a32c81f5476c5a8a77108
Zhan Gevorgiz <zhangevorgiz@gmail.com>
2024-08-10 09:03:13 +0300

    Добавлен Getter Setter

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -4,6 +4,9 @@ public class MainApplication {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("New Hello world");
+        System.out.println("1000");
+        System.out.println("2222 Hello world");
+        System.out.println("3333 Hello world");
+
     }
 }
---
+++ src/main/java/com/stepup/proj/box.java
@@ -0,0 +1,20 @@
+package com.stepup.proj;public class box {
+
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public box(int size) {
+        this.size = size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+
+
+    }
+}
+
+
